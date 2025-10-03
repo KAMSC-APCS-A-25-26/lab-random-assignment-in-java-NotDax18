4d7a5b7622215289f6ee674c4f6b8779e3e4fd1b
NotDax18 <dax.cw.robinson@gmail.com>
2025-10-03 13:44:45 -0400

    Update PhoneNumberGenerator.java

--- src/main/java/PhoneNumberGenerator.java
+++ src/main/java/PhoneNumberGenerator.java
@@ -15,5 +15,19 @@ public class PhoneNumberGenerator {
         
         // TODO: Format and display phone number as XXX-XXX-XXX
         // Include the dashes in the output
+        Random rando = new Random();
+        // First three digits: each digit 0-7 (no 8 or 9)
+        int d1 = rando.nextInt(8);
+        int d2 = rando.nextInt(8);
+        int d3 = rando.nextInt(8);
+        String firstGroup = "" + d1 + d2 + d3;
+        // Second three digits: 000-742
+        int secondGroupNum = rando.nextInt(743); // 0 to 742 inclusive
+        String secondGroup = String.format("%03d", secondGroupNum);
+        // Third three digits: 000-999
+        int thirdGroupNum = rando.nextInt(1000);
+        String thirdGroup = String.format("%03d", thirdGroupNum);
+        // Display in XXX-XXX-XXX format
+        System.out.println(firstGroup + "-" + secondGroup + "-" + thirdGroup);
     }
 }
